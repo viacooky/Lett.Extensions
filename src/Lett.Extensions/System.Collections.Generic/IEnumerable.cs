29072c2825b3fd68b0b7f3be078e979c31689ddb
viacooky <viacooky@qq.com>
2019-04-11 13:27:50 +0800

    [UPDATE] 废除错误的IEnumerable拓展方法ForEach<T>

--- src/Lett.Extensions/System.Collections.Generic/IEnumerable.cs
+++ src/Lett.Extensions/System.Collections.Generic/IEnumerable.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Lett.Extensions
 {
     public static partial class Extensions
     {
-        /// <summary>
-        ///     拓展ForEach方法
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="this"></param>
-        /// <param name="action"></param>
-        public static void ForEach<T>(this IEnumerable<T> @this, Action<T> action)
-        {
-            @this.ToList().ForEach(action);
-        }
-
         /// <summary>
         ///     是否空或空集合
         /// </summary>
