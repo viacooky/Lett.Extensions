9825d87e5209e498500b8cf1b83bc2e5b9439f2f
viacooky <viacooky@qq.com>
2019-04-11 13:29:49 +0800

    [UP] 调整Enum拓展方法的返回
    将异常时返回String.Empty改为返回null，避免异义

--- src/Lett.Extensions/System.Enum/Enum.cs
+++ src/Lett.Extensions/System.Enum/Enum.cs
@@ -9,30 +9,32 @@ namespace Lett.Extensions
         ///     获取枚举描述
         ///     需要在枚举中加入 Description 属性
         ///     [Description("xxx")]
+        ///     异常时 返回null
         /// </summary>
         /// <param name="this"></param>
-        /// <returns></returns>
+        /// <returns>  </returns>
         public static string GetDescription(this Enum @this)
         {
             var enumType = @this.GetType();
             var enumName = Enum.GetName(enumType, @this);
-            if (enumName == null) return string.Empty;
+            if (enumName == null) return null;
             var fieldInfo = enumType.GetField(enumName);
-            if (fieldInfo == null) return string.Empty;
+            if (fieldInfo == null) return null;
             return Attribute.GetCustomAttribute(fieldInfo, typeof(DescriptionAttribute), false) is DescriptionAttribute descAttr
                 ? descAttr.Description
-                : string.Empty;
+                : null;
         }
 
         /// <summary>
         ///     int 获取枚举的描述
+        ///     异常时 返回null
         /// </summary>
         /// <param name="this"></param>
-        /// <param name="enumType"></param>
+        /// <param name="enumType">枚举类型，枚举类型不存在时，返回空字符串</param>
         /// <returns></returns>
         public static string GetEnumDescription(this int @this, Type enumType)
         {
-            if (!enumType.IsEnum) return string.Empty;
+            if (!enumType.IsEnum) return null;
             var enumValue = Enum.ToObject(enumType, @this);
             return ((Enum) enumValue).GetDescription();
         }
