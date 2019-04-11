8b5da3fedfa4639bc3cd6f9f41b73be45d2b3d1f
viacooky <viacooky@qq.com>
2019-04-12 02:48:02 +0800

    [UPDATE] 增加单元测试

---
+++ src/Lett.Extensions.Test/System.String/String.Compare.Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lett.Extensions.Test
+{
+    [TestClass]
+    public class StringCompareTest
+    {
+        [TestMethod]
+        public void IsEmail_Test()
+        {
+            Assert.IsTrue("abc@qqq.com".IsEmail());
+            Assert.IsFalse("abc@qqq#.com".IsEmail());
+            Assert.IsTrue("abc@qqqabc.acdk.com".IsEmail());
+            Assert.IsFalse("abc@qqqabc@acdk.com".IsEmail());
+        }
+
+        [TestMethod]
+        public void IsUpper_Test()
+        {
+            Assert.IsTrue("ABC".IsUpper());
+            Assert.IsFalse("ABd".IsUpper());
+            Assert.IsFalse("ccc".IsUpper());
+        }
+
+        [TestMethod]
+        public void IsLower_Test()
+        {
+            Assert.IsTrue("abcddd".IsLower());
+            Assert.IsFalse("abDD".IsLower());
+            Assert.IsFalse("DDDD".IsLower());
+        }
+
+        [TestMethod]
+        public void IsUrl_Test()
+        {
+            Assert.IsTrue("http://sdf.com".IsUrl());
+            Assert.IsTrue("https://sdf.com".IsUrl());
+            Assert.IsTrue("ftp://sdf.com".IsUrl());
+            Assert.IsTrue("http://sdf.c.dddd.cccc.saaa.com".IsUrl());
+            Assert.IsTrue("aaaadd://sdf.com".IsUrl());
+            Assert.IsFalse("/sdf.com".IsUrl());
+            Assert.IsFalse("www.sdf.com".IsUrl());
+            Assert.IsFalse("www.asdfwww.sdf.com".IsUrl());
+        }
+
+        [TestMethod]
+        public void IgnoreCaseEquals_Test()
+        {
+            Assert.IsTrue("aaa".IgnoreCaseEquals("AaA"));
+        }
+    }
+}
---
+++ src/Lett.Extensions.Test/System.String/String.Encrypt.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lett.Extensions.Test
+{
+    [TestClass]
+    public class StringEncryptTest
+    {
+        [TestMethod]
+        public void Base64_Test()
+        {
+            var str1   = "ABCD";
+            var base64 = "QUJDRA==";
+            Assert.AreEqual(str1.Base64Encode(), base64); // encode
+            Assert.AreEqual(base64.Base64Decode(), str1); // decode
+        }
+    }
+}
