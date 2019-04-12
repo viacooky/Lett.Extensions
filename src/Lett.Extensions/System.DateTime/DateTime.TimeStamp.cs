dcf1811d5a39beb0670bda8da3cf999fb4b2dbc2
viacooky <viacooky@qq.com>
2019-04-13 02:21:34 +0800

    [ADD] 增加时间戳拓展方法

---
+++ src/Lett.Extensions/System.DateTime/DateTime.TimeStamp.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Lett.Extensions
+{
+    public static partial class Extensions
+    {
+        private static readonly DateTime OriginDateTime = new DateTime(1970, 1, 1, 0, 0, 0);
+
+        /// <summary>
+        ///     获取时间戳的日表示
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static double GetTotalDays(this DateTime @this)
+        {
+            return (@this - OriginDateTime).TotalDays;
+        }
+
+        /// <summary>
+        ///     获取时间戳的小时表示
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static double GetTotalHours(this DateTime @this)
+        {
+            return (@this - OriginDateTime).TotalHours;
+        }
+
+        /// <summary>
+        ///     获取时间戳的分钟表示
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static double GetTotalMinutes(this DateTime @this)
+        {
+            return (@this - OriginDateTime).TotalMinutes;
+        }
+
+        /// <summary>
+        ///     获取时间戳的秒表示
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static double GetTotalSeconds(this DateTime @this)
+        {
+            return (@this - OriginDateTime).TotalSeconds;
+        }
+
+        /// <summary>
+        ///     获取时间戳的毫秒表示
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static double GetTotalMilliseconds(this DateTime @this)
+        {
+            return (@this - OriginDateTime).TotalMilliseconds;
+        }
+    }
+}
