058c2681cde6e7ad5eeee2c2d645c9b665d5cc91
viacooky <viacooky@qq.com>
2019-04-24 14:51:03 +0800

    feat: 增加 Object.To<T>() 方法的泛型约束为 IConvertible
    增加 Object.To<T>() 方法的泛型约束为 实现了 IConvertible 接口的类型
    如：Boolean, Byte, Char, DateTime, System.DBNull, Decimal, Double, System.Enum, Int16, Int32, Int64, SByte, Single, String, UInt16, UInt32, UInt64 等

--- src/Lett.Extensions/System.Object/Object.Convert.cs
+++ src/Lett.Extensions/System.Object/Object.Convert.cs
@@ -10,36 +10,45 @@ namespace Lett.Extensions
         #region 对象转换
 
         /// <summary>
-        ///     对象转换
+        ///     对象转换,失败则返回 func (泛型约束为实现了IConvertible的类型)
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">
+        ///     泛型约束为实现了IConvertible的类型(Boolean,Byte,Char,DateTime,System.DBNull,Decimal,Double,System.Enum,Int16,Int32,Int64,SByte,Single,String,UInt16,UInt32,UInt64
+        ///     等)
+        /// </typeparam>
         /// <param name="this"></param>
         /// <returns></returns>
-        public static T To<T>(this object @this)
+        public static T To<T>(this object @this) where T : IConvertible
         {
             return @this.To(default(T));
         }
 
         /// <summary>
-        ///     对象转换
+        ///     对象转换,失败则返回 func (泛型约束为实现了IConvertible的类型)
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">
+        ///     泛型约束为实现了IConvertible的类型(Boolean,Byte,Char,DateTime,System.DBNull,Decimal,Double,System.Enum,Int16,Int32,Int64,SByte,Single,String,UInt16,UInt32,UInt64
+        ///     等)
+        /// </typeparam>
         /// <param name="this"></param>
         /// <param name="func"></param>
         /// <returns></returns>
-        public static T To<T>(this object @this, Func<T> func)
+        public static T To<T>(this object @this, Func<T> func) where T : IConvertible
         {
             return @this.To(func.Invoke());
         }
 
         /// <summary>
-        ///     对象转换
+        ///     对象转换,失败则返回 defaultValue (泛型约束为实现了IConvertible的类型)
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">
+        ///     泛型约束为实现了IConvertible的类型(Boolean,Byte,Char,DateTime,System.DBNull,Decimal,Double,System.Enum,Int16,Int32,Int64,SByte,Single,String,UInt16,UInt32,UInt64
+        ///     等)
+        /// </typeparam>
         /// <param name="this"></param>
         /// <param name="defaultValue"></param>
         /// <returns></returns>
-        public static T To<T>(this object @this, T defaultValue)
+        public static T To<T>(this object @this, T defaultValue) where T : IConvertible
         {
             if (@this == null || @this == DBNull.Value) return defaultValue;
             if (typeof(T).IsEnum)
