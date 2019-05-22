3a9439c68f9a4aac547b3ef7a6408a278fb46757
viacooky <viacooky@qq.com>
2019-05-22 20:11:03 +0800

    docs: ByteExtensions docs

--- src/Lett.Extensions/System.Byte/Bytes.Convert.cs
+++ src/Lett.Extensions/System.Byte/Bytes.Convert.cs
@@ -1,9 +1,10 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace Lett.Extensions
 {
     /// <summary>
-    ///     Bytes 扩展方法 - 转换
+    ///     Bytes 扩展方法
     /// </summary>
     public static class ByteExtensions
     {
@@ -11,8 +12,25 @@ namespace Lett.Extensions
         ///     转换为字符串
         /// </summary>
         /// <param name="this"></param>
-        /// <param name="encoding">编码格式</param>
+        /// <param name="encoding">
+        ///     <para>编码格式</para>
+        ///     <para>为空时，默认Encoding.UTF8</para>
+        /// </param>
         /// <returns></returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="this" />
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="this" />
+        /// </exception>
+        /// <exception cref="DecoderFallbackException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// bytes.ToString(Encoding.UTF8);
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString(this byte[] @this, Encoding encoding)
         {
             encoding = encoding ?? Encoding.UTF8;
