e8145984a0c366b1ad759c7218a8f53e4599e8cc
viacooky <viacooky@qq.com>
2019-05-22 22:49:58 +0800

    docs: DateTimeExtensions

--- src/Lett.Extensions/System.DateTime/DateTime.Convert.cs
+++ src/Lett.Extensions/System.DateTime/DateTime.Convert.cs
@@ -3,7 +3,7 @@ using System;
 namespace Lett.Extensions
 {
     /// <summary>
-    ///     DateTime 扩展方法 - 转换
+    ///     DateTime 扩展方法
     /// </summary>
     public static partial class DateTimeExtensions
     {
@@ -15,9 +15,17 @@ namespace Lett.Extensions
         /// <param name="minute">分钟 (0-59) </param>
         /// <param name="second">秒 (0-59) </param>
         /// <param name="millisecond">毫秒 (0-999) 默认0</param>
-        /// <returns></returns>
-        /// <exception cref="System.ArgumentOutOfRangeException">en:Hour, Minute, and Second parameters describe an un-representable DateTime. (小时、分钟和秒参数描述了一个无法表示的日期时间。)</exception>
-        /// <exception cref="System.ArgumentOutOfRangeException">Valid values are between 0 and 999, inclusive. (millisecond 的有效值在0和999之间。)</exception>
+        /// <returns>返回一个新的<see cref="DateTime" /></returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="hour" />、<paramref name="minute" /> 和 <paramref name="second" /> 参数描述了一个无法表示的日期时间</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="millisecond" /> 的有效值不在0和999之间</exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt2 = new DateTime(2019, 4, 1, 1, 2, 3);
+        /// var rs2 = dt2.SetTime(23, 11, 11, 999);
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static DateTime SetTime(this DateTime @this, int hour, int minute, int second, int millisecond = 0)
         {
             return new DateTime(@this.Year, @this.Month, @this.Day, hour, minute, second, millisecond);
@@ -27,7 +35,16 @@ namespace Lett.Extensions
         ///     获取 DateTime 的开始 (year-month-day 00:00:00.000)
         /// </summary>
         /// <param name="this"></param>
-        /// <returns></returns>
+        /// <returns>返回一个新的<see cref="DateTime" /></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// var rs = dt.StartOfDay(); // rs == new DateTime(2019, 4, 1, 0, 0, 0, 000)
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static DateTime StartOfDay(this DateTime @this)
         {
             return @this.SetTime(0, 0, 0);
@@ -37,7 +54,16 @@ namespace Lett.Extensions
         ///     获取 DateTime 的结束 (year-month-day 23:59:59.999)
         /// </summary>
         /// <param name="this"></param>
-        /// <returns></returns>
+        /// <returns>返回一个新的<see cref="DateTime" /></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 1, 2, 3);
+        /// var rs = dt.EndOfDay(); // rs == new DateTime(2019, 4, 1, 23, 59, 59, 999);
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static DateTime EndOfDay(this DateTime @this)
         {
             return @this.SetTime(23, 59, 59, 999);
@@ -49,7 +75,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <param name="startOfWeek">设置每周的起始</param>
-        /// <returns></returns>
+        /// <returns>返回一个新的<see cref="DateTime" /></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 1, 2, 3);
+        /// var rs = dt.StartOfWeek(DayOfWeek.Sunday); // 2019-03-31 00:00:00
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static DateTime StartOfWeek(this DateTime @this, DayOfWeek startOfWeek)
         {
             var diff = @this.DayOfWeek - startOfWeek;
@@ -61,7 +96,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <param name="startOfWeek">设置每周的起始</param>
-        /// <returns></returns>
+        /// <returns>返回一个新的<see cref="DateTime" /></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt2 = new DateTime(2019, 4, 1, 1, 2, 3);
+        /// var rs2 = dt.EndOfWeek(DayOfWeek.Friday); // 2019-04-04 23:59:59.999
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static DateTime EndOfWeek(this DateTime @this, DayOfWeek startOfWeek)
         {
             return @this.StartOfWeek(startOfWeek).AddDays(6).EndOfDay();
--- src/Lett.Extensions/System.DateTime/DateTime.Formatter.cs
+++ src/Lett.Extensions/System.DateTime/DateTime.Formatter.cs
@@ -12,6 +12,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_Year(); // "2019"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_Year(this DateTime @this)
         {
             return @this.ToString("yyyy");
@@ -22,6 +32,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_ShortYear(); // "19"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_ShortYear(this DateTime @this)
         {
             return @this.ToString("yy");
@@ -32,6 +52,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_Month(); // "2019-04"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_Month(this DateTime @this)
         {
             return @this.ToString("yyyy-MM");
@@ -42,6 +72,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_ShortMonth(); // "19-4"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_ShortMonth(this DateTime @this)
         {
             return @this.ToString("yy-M");
@@ -52,6 +92,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_Day(); // "2019-04-01"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_Day(this DateTime @this)
         {
             return @this.ToString("yyyy-MM-dd");
@@ -62,6 +112,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_ShortDay(); // "19-4-1"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_ShortDay(this DateTime @this)
         {
             return @this.ToString("yy-M-d");
@@ -72,6 +132,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_Time(); // "21:11:11"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_Time(this DateTime @this)
         {
             return @this.ToString("HH:mm:ss");
@@ -83,6 +153,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_ShortTime(); // "09:11:11 PM"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_ShortTime(this DateTime @this)
         {
             return @this.ToString("hh:mm:ss tt");
@@ -93,6 +173,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_Base(); // "2019-04-01 21:11:11"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_Base(this DateTime @this)
         {
             return @this.ToString("yyyy-MM-dd HH:mm:ss");
@@ -104,6 +194,16 @@ namespace Lett.Extensions
         /// </summary>
         /// <param name="this"></param>
         /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dt = new DateTime(2019, 4, 1, 21, 11, 11, 123);
+        /// dt.ToString_Full(); // "2019-04-01 21:11:11.1230000"
+        ///         ]]>
+        ///     </code>
+        /// </example>
         public static string ToString_Full(this DateTime @this)
         {
             return @this.ToString("yyyy-MM-dd HH:mm:ss.fffffff");
