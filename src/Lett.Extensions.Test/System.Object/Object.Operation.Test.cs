e441daa932a96b7595511da289c5b386bcd5236a
viacooky <viacooky@qq.com>
2019-05-26 18:26:16 +0800

    fix: fix error test

--- src/Lett.Extensions.Test/System.Object/Object.Operation.Test.cs
+++ src/Lett.Extensions.Test/System.Object/Object.Operation.Test.cs
@@ -10,22 +10,22 @@ namespace Lett.Extensions.Test
         public void Pipe_Test()
         {
             var obj1 = new MyClass();
-            var rs = obj1.Pipe(o => { o.Name += "pipe1";})
-                         .Pipe(o => { o.Name += "_pipe2";})
-                         .Pipe(o => { o.Name += "_pipe3";})
-                         .Pipe(o => { o.Name += "_pipe4";})
-                         .Pipe(o => { o.Name += "_pipe5";});
-            
-            Assert.AreEqual(rs.Name,"pipe1_pipe2_pipe3_pipe4_pipe5");
+            var rs = obj1.Pipe(o => { o.Name += "pipe1"; })
+                         .Pipe(o => { o.Name += "_pipe2"; })
+                         .Pipe(o => { o.Name += "_pipe3"; })
+                         .Pipe(o => { o.Name += "_pipe4"; })
+                         .Pipe(o => { o.Name += "_pipe5"; });
 
-            var rs2 = obj1.Pipe(o => { o.Name += "pipe1";})
-                         .Pipe(o => { o.Name += "_pipe2";})
-                         .Pipe(o => { o.Name += "_pipe3";})
-                         .Pipe(o => { o.Name += "_pipe4";})
-                         .Pipe(o => { o.Name += "_pipe5";})
-                         .Pipe(o => o.Name.Replace("pipe", "")); // pipe func
+            Assert.AreEqual(rs.Name, "pipe1_pipe2_pipe3_pipe4_pipe5");
+
+            obj1 = new MyClass();
+            var rs2 = obj1.Pipe(o => { o.Name += "pipe1"; })
+                          .Pipe(o => { o.Name += "_pipe2"; })
+                          .Pipe(o => { o.Name += "_pipe3"; })
+                          .Pipe(o => { o.Name += "_pipe4"; })
+                          .Pipe(o => { o.Name += "_pipe5"; })
+                          .Pipe(o => o.Name.Replace("pipe", "")); // pipe func
             Assert.AreEqual(rs2, "1_2_3_4_5");
-
         }
 
         private class MyClass
