5d192916a5bdb58eeb319fb503256eec86f2a330
viacooky <viacooky@qq.com>
2019-08-15 00:01:32 +0800

    feat: `ICollection` 增加 `AddRange` `AddRangeParams`

--- src/Lett.Extensions.Test/System.Collections.Generic/ICollections.Test.cs
+++ src/Lett.Extensions.Test/System.Collections.Generic/ICollections.Test.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lett.Extensions.Test
@@ -29,6 +30,55 @@ namespace Lett.Extensions.Test
             Assert.AreEqual(list[0], "aa");
             Assert.AreEqual(list[1], "bb");
             Assert.AreEqual(list[2], "cc");
+
+            var arr    = new[] {"aa", "bb"};
+            var addArr = new[] {"cc"};
+            Assert.ThrowsException<NotSupportedException>(() => arr.AddIfNotContains(addArr));
+        }
+
+        [TestMethod]
+        public void AddIfNotContainsParams_Test()
+        {
+            var list = new List<string> {"aa", "bb"};
+            list.AddIfNotContainsParams("cc", "aa");
+            Assert.AreEqual(list.Count, 3);
+            Assert.AreEqual(list[0], "aa");
+            Assert.AreEqual(list[1], "bb");
+            Assert.AreEqual(list[2], "cc");
+
+            var arr = new[] {"aa", "bb"};
+            Assert.ThrowsException<NotSupportedException>(() => arr.AddIfNotContainsParams("cc", "dd"));
+        }
+
+        [TestMethod]
+        public void AddRange_Test()
+        {
+            var list       = new List<string> {"aa", "bb"};
+            var appendList = new List<string> {"cc", "aa"};
+            list.AddRange(appendList);
+            Assert.AreEqual(list.Count, 4);
+            Assert.AreEqual(list[0], "aa");
+            Assert.AreEqual(list[1], "bb");
+            Assert.AreEqual(list[2], "cc");
+            Assert.AreEqual(list[3], "aa");
+
+            var arr = new[] {"aa", "bb"};
+            Assert.ThrowsException<NotSupportedException>(() => arr.AddRange(appendList));
+        }
+
+        [TestMethod]
+        public void AddRangeParams_Test()
+        {
+            var list       = new List<string> {"aa", "bb"};
+            list.AddRangeParams("cc", "aa");
+            Assert.AreEqual(list.Count, 4);
+            Assert.AreEqual(list[0], "aa");
+            Assert.AreEqual(list[1], "bb");
+            Assert.AreEqual(list[2], "cc");
+            Assert.AreEqual(list[3], "aa");
+            
+            var arr = new[] {"aa", "bb"};
+            Assert.ThrowsException<NotSupportedException>(() => arr.AddRangeParams("cc", "aa"));
         }
     }
 }
--- src/Lett.Extensions/System.Collections.Generic/ICollections.cs
+++ src/Lett.Extensions/System.Collections.Generic/ICollections.cs
@@ -15,9 +15,7 @@ namespace Lett.Extensions
         /// <param name="this"></param>
         /// <param name="item">匹配项</param>
         /// <typeparam name="T">集合中元素的类型</typeparam>
-        /// <exception cref="NotSupportedException">
-        ///     <paramref name="this" />
-        /// </exception>
+        /// <exception cref="NotSupportedException"> <paramref name="this" /> maybe is read-only</exception>
         /// <example>
         ///     <code>
         ///         <![CDATA[
@@ -39,9 +37,7 @@ namespace Lett.Extensions
         /// <param name="this"></param>
         /// <param name="items">匹配项集合</param>
         /// <typeparam name="T">集合中元素的类型</typeparam>
-        /// <exception cref="NotSupportedException">
-        ///     <paramref name="this" />
-        /// </exception>
+        /// <exception cref="NotSupportedException"> <paramref name="this" /> maybe is read-only</exception>
         /// <example>
         ///     <code>
         ///         <![CDATA[
@@ -52,10 +48,78 @@ namespace Lett.Extensions
         ///         ]]>
         ///     </code>
         /// </example>
-        public static void AddIfNotContains<T>(this ICollection<T> @this, ICollection<T> items)
+        public static void AddIfNotContains<T>(this ICollection<T> @this, IEnumerable<T> items)
         {
             if (@this == null || items == null) return;
             foreach (var item in items) @this.AddIfNotContains(item);
+        }
+
+        /// <summary>
+        ///     如果不包含，则添加
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="items">匹配项集合</param>
+        /// <typeparam name="T">集合中元素的类型</typeparam>
+        /// <exception cref="NotSupportedException"> <paramref name="this" /> maybe is read-only</exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var list = new List<string> {"aa", "bb"};
+        /// list.AddIfNotContains("cc", "aa");
+        /// // {"aa", "bb", "cc"}
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static void AddIfNotContainsParams<T>(this ICollection<T> @this, params T[] items)
+        {
+            @this.AddIfNotContains(items);
+        }
+
+        /// <summary>
+        ///     批量添加
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="items"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="NotSupportedException"> <paramref name="this" /> maybe is read-only</exception>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="this" />
+        /// </exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var list       = new List<string> {"aa", "bb"};
+        /// var appendList = new List<string> {"cc", "aa"};
+        /// list.AddRange(appendList);
+        /// // {"aa", "bb", "cc", "aa"}
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static void AddRange<T>(this ICollection<T> @this, IEnumerable<T> items)
+        {
+            if (@this.IsNull()) throw new ArgumentNullException(nameof(@this), "is null");
+            foreach (var item in items) @this.Add(item);
+        }
+
+        /// <summary>
+        ///     批量添加
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="items"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="NotSupportedException"> <paramref name="this" /> maybe is read-only</exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var list       = new List<string> {"aa", "bb"};
+        /// list.AddRange("cc", "aa");
+        /// // {"aa", "bb", "cc", "aa"}
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static void AddRangeParams<T>(this ICollection<T> @this, params T[] items)
+        {
+            @this.AddRange(items);
         }
     }
 }
