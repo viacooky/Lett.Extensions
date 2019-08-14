bf74abf936e0e5034e360c2cf6ac1387207dc0cd
viacooky <viacooky@qq.com>
2019-08-14 23:59:46 +0800

    feat: `IDictionary` 增加 `AddOrUpdate` `GetOrUpdate`

--- src/Lett.Extensions/System.Collections.Generic/IDictionary.Opertion.cs
+++ src/Lett.Extensions/System.Collections.Generic/IDictionary.Opertion.cs
@@ -10,14 +10,15 @@ namespace Lett.Extensions
     public static partial class IDictionaryExtensions
     {
         /// <summary>
-        ///     添加或设置
+        ///     <para>添加或更新</para>
+        ///     <para>当 <paramref name="this" /> 中不存在 <paramref name="pair" /> 的 <c>Key</c> 时，将 <paramref name="pair" /> 的 <c>Value</c> 添加到 <paramref name="this" /> 中</para>
         /// </summary>
         /// <param name="this"></param>
         /// <param name="pair">键值对</param>
         /// <typeparam name="TKey"></typeparam>
         /// <typeparam name="TValue"></typeparam>
         /// <exception cref="ArgumentNullException">key is null</exception>
-        /// <exception cref="NotSupportedException"><paramref name="this" />maybe is read-only</exception>
+        /// <exception cref="NotSupportedException"><paramref name="this" /> maybe is read-only</exception>
         /// <example>
         ///     <code>
         ///         <![CDATA[
@@ -28,13 +29,14 @@ namespace Lett.Extensions
         ///         ]]>
         ///     </code>
         /// </example>
-        public static void AddOrSet<TKey, TValue>(this IDictionary<TKey, TValue> @this, KeyValuePair<TKey, TValue> pair)
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> @this, KeyValuePair<TKey, TValue> pair)
         {
-            @this.AddOrSet(pair.Key, pair.Value);
+            @this.AddOrUpdate(pair.Key, pair.Value);
         }
 
         /// <summary>
-        ///     添加或设置
+        ///     <para>添加或更新</para>
+        ///     <para>当 <paramref name="this" /> 中不存在 <paramref name="key" /> 时，将 {<paramref name="key" />, <paramref name="value" />} 添加到 <paramref name="this" /> 中</para>
         /// </summary>
         /// <param name="this"></param>
         /// <param name="key">键</param>
@@ -42,7 +44,7 @@ namespace Lett.Extensions
         /// <typeparam name="TKey"></typeparam>
         /// <typeparam name="TValue"></typeparam>
         /// <exception cref="ArgumentNullException">key is null</exception>
-        /// <exception cref="NotSupportedException"><paramref name="this" />maybe is read-only</exception>
+        /// <exception cref="NotSupportedException"><paramref name="this" /> maybe is read-only</exception>
         /// <example>
         ///     <code>
         ///         <![CDATA[
@@ -53,10 +55,39 @@ namespace Lett.Extensions
         ///         ]]>
         ///     </code>
         /// </example>
-        public static void AddOrSet<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue value)
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue value)
         {
             if (@this.ContainsKey(key)) @this[key] = value;
             else @this.Add(key, value);
+        }
+
+        /// <summary>
+        ///     <para>获取值或更新</para>
+        ///     <para>当 <paramref name="this" /> 中不存在 <paramref name="key" /> 时，将 {<paramref name="key" />, <paramref name="value" />} 添加到 <paramref name="this" /> 中，并返回 <paramref name="value" /></para>
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">key is null</exception>
+        /// <exception cref="NotSupportedException"><paramref name="this" />maybe is read-only</exception>
+        /// <example>
+        ///     <code>
+        ///         <![CDATA[
+        /// var dict = new Dictionary<string, int> {{"a", 1}, {"b", 2}};
+        /// var rs = dict.GetOrUpdate("c", 3);
+        /// // rs = 3;
+        /// // dict = {{"a", 1}, {"b", 2}, {"c", 3}};
+        ///         ]]>
+        ///     </code>
+        /// </example>
+        public static TValue GetOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue value)
+        {
+            if (@this.ContainsKey(key)) return @this[key];
+            @this.Add(key, value);
+            return value;
         }
     }
 }
